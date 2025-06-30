a3021c399ec60c13dc04eb8b08175b135b653348
DuM1h <dumih00@gmail.com>
2025-06-30 20:13:22 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int main(){
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	printf("Введіть три значення:\n");
+	int t1, t2, t3;
+	scanf("%d %d %d", &t1, &t2, &t3);
+	
+	if (t1>10000||t2>10000||t3>10000){
+		printf("Числа не повинні перевищувати 10000");
+		return 1;
+	}
+	
+	float t = (t1+t2+t3)/3.0;
+	printf("Час, необхідний для з'їдання пирога %.2f", t);
+	
+	return 0;
+}
